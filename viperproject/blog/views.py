cf32a78a5adf0452098169d6fd67205d0c29f187
Abdulkareem Olawale <olawaleabiola741@gmail.com>
2022-08-04 01:49:38 +1300

    python templates

--- viperproject/blog/views.py
+++ viperproject/blog/views.py
@@ -1,9 +1,22 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Products
 
 # Create your views here.
-def index(request):
+
+def about(request):
     #return HttpResponse('Hello World!')
+    from django.core import serializers
+    
     products = Products.objects.all()
-    return render(request, 'index.html', {'items': products})
+    data = serializers.serialize('python', products)
+
+    context = {
+        'data' : data
+    }
+
+    return render(request, 'about.html', context)
+
+
+
--- viperproject/viperproject/urls.py
+++ viperproject/viperproject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from itertools import product
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('blog.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
